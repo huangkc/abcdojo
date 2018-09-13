254076131a014a18a0a326776a284f994af913dd
Kai Huang <kaihuang@kais-air.attlocal.net>
2018-09-12 18:54:03 -0700

    import files

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,87 @@
+get '/home' do
+  erb :'home/index'
+end
+
+get '/aikido' do
+	erb :'home/aikido'
+end
+
+get '/about' do
+	erb :'home/about'
+end
+
+get '/instructors' do
+	erb :'home/instructors'	
+end
+
+get '/schedule' do
+	erb :'home/schedule'	
+end
+
+get '/fees' do
+	erb :'home/fees'	
+end
+
+get '/adult_class' do
+	erb :'home/adult_class'	
+end
+
+get '/children_class' do
+	erb :'home/children_class'	
+end
+
+get '/weapons' do
+	erb :'home/weapons'	
+end
+
+get '/iaido' do
+	erb :'home/iaido'	
+end
+
+get '/events' do
+	erb :'home/events'
+end
+
+get '/etiquette' do
+	erb :'home/etiquette'
+end
+
+get '/newstudent' do
+	erb :'home/newstudent'
+end
+
+get '/testing' do
+	erb :'home/testing'
+end
+
+get '/media' do
+	erb :'home/media'
+end
+
+get '/directions' do
+	erb :'home/directions'
+end
+
+get '/contact' do
+	erb :'home/contact'
+end
+
+get '/blog' do 
+	erb :'home/blog' 
+end
+
+get '/ki' do 
+	erb :'home/ki' 
+end
+
+get '/special' do
+	erb :'home/special'
+end
+
+get '/privatelesson' do 
+	erb :'home/privatelesson'
+end
+
+get '/driving' do 
+	erb :'home/driving'
+end
